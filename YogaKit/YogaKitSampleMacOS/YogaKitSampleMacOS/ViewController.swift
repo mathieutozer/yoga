598a5e693837e3e0b0968ca16ebcc3b878fb8f26
Mathieu Tozer <mathieutozer@gmail.com>
2017-04-11 11:50:05 -0400

    Added lauyout to the base (a port of BasicViewController)

--- YogaKit/YogaKitSampleMacOS/YogaKitSampleMacOS/ViewController.swift
+++ YogaKit/YogaKitSampleMacOS/YogaKitSampleMacOS/ViewController.swift
@@ -7,21 +7,61 @@
 //
 
 import Cocoa
+import YogaKit
+
+class BackgroundView: NSView {
+  var backgroundColor: NSColor = .white
+
+  override func draw(_ dirtyRect: NSRect) {
+    super.draw(dirtyRect)
+    backgroundColor.set()
+    NSRectFill(dirtyRect)
+  }
+}
 
 class ViewController: NSViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    let containerSize = self.view.bounds.size
 
-    // Do any additional setup after loading the view.
-  }
-
-  override var representedObject: Any? {
-    didSet {
-    // Update the view, if already loaded.
+    let root = self.view
+    root.configureLayout { (layout) in
+      layout.isEnabled = true
+      layout.width = containerSize.width
+      layout.height = containerSize.height
+      layout.alignItems = .center
+      layout.justifyContent = .center
     }
-  }
 
+    let child1 = BackgroundView()
+    child1.backgroundColor = .blue
+    child1.configureLayout { (layout) in
+      layout.isEnabled = true
+      layout.width = 100
+      layout.height = 10
+      layout.marginBottom = 25
+    }
+    root.addSubview(child1)
+
+    let child2 = BackgroundView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+    child2.backgroundColor = .green
+    child2.configureLayout { (layout) in
+      layout.isEnabled = true
+      layout.alignSelf = .flexEnd
+    }
+    root.addSubview(child2)
+
+    let child3 = BackgroundView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+    child3.backgroundColor = .yellow
+    child3.configureLayout { (layout) in
+      layout.isEnabled = true
+      layout.alignSelf = .flexStart
+    }
+    root.addSubview(child3)
+
+    root.yoga.applyLayout(preservingOrigin: true)
+  }
 
 }
 
